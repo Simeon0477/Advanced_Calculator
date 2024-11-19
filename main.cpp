b47271ffe8fdfd665aea4a7add0dae3a4d4b4985
Siméon0477 <nouhoumimeon01@gmail.com>
2024-11-19 10:42:58 +0100

    commit 3

--- main.cpp
+++ main.cpp
@@ -7,24 +7,26 @@ using namespace std;
 
 void operation(char *operation);
 double calcul_operation(string operation[50]);
-int search(int position, char sens);
+int search(int pos, char sens);
 
 int n = 1;
 string chaine;
 string calcul[50];
+double result;
 
-int main() {
+int main(int argc, char** args){
     cout << "Entrez votre operation : " << endl;
     cin >> chaine;
     char* chaine2 = const_cast<char*>(chaine.c_str());
     operation(chaine2);
     int i=0;
-    //while(calcul[i] != ""){
-    //    cout << calcul[i] << endl;
-    //    i++;
-    //}
+    while(calcul[i] != ""){
+        cout << calcul[i] << endl;
+        i++;
+    }
+    result = calcul_operation(calcul);
 
-    cout << "Le resultat est : " << calcul_operation(calcul) << endl;
+    cout << "Le resultat est : " << result << endl;
 }
 
 void operation(char *operation){
@@ -52,58 +54,64 @@ void operation(char *operation){
 double calcul_operation(string operation[50]){
     double result;
     int i=0;
+
     while(calcul[i] != ""){
-        if(calcul[i] == " "){
+        if(calcul[i] == "_"){
             continue;
         }else if(calcul[i] == "*"){
             result = atof(calcul[search(i, 'd')].c_str()) * atof(calcul[search(i, 'a')].c_str());
             calcul[search(i, 'd')] = to_string(result);
-            calcul[i] = " ";
-            calcul[search(i, 'a')] = " ";
+            calcul[i] = "_";
+            calcul[search(i, 'a')] = "_";
         }else if(calcul[i] == "/"){
             if(atof(calcul[search(i, 'a')].c_str()) == 0){
                 cout << "Math error" << endl;
             }else{
                 result = atof(calcul[search(i, 'd')].c_str()) / atof(calcul[search(i, 'a')].c_str());
                 calcul[search(i, 'd')] = to_string(result);
-                calcul[i] = " ";
-                calcul[search(i, 'a')] = " ";
+                calcul[i] = "_";
+                calcul[search(i, 'a')] = "_";
             }
         }
         else if(calcul[i] == "+"){
             result = atof(calcul[search(i, 'd')].c_str()) + atof(calcul[search(i, 'a')].c_str());
             calcul[search(i, 'd')] = to_string(result);
-            calcul[i] = " ";
-            calcul[search(i, 'a')] = " ";
+            calcul[i] = "_";
+            calcul[search(i, 'a')] = "_";
         }
         else if(calcul[i] == "-"){
             result = atof(calcul[search(i, 'd')].c_str()) - atof(calcul[search(i, 'a')].c_str());
             calcul[search(i, 'd')] = to_string(result);
-            calcul[i] = " ";
-            calcul[search(i, 'a')] = " ";
+            calcul[i] = "_";
+            calcul[search(i, 'a')] = "_";
         }
+
         i++;
     }
+
     return atof(calcul[0].c_str());
 }
 
-int search(int position, char sens){
+int search(int pos, char sens){
+    int position = pos;
     if(sens == 'a'){
         while(calcul[position] != ""){
-            if(calcul[position] == " "){
+            if(calcul[position] == "_"){
                 continue;
             }else{
                 return position;
             }
+
             position++;
         }
     }else if(sens == 'd'){
         while(position >= 0){
-            if(calcul[position] == " "){
+            if(calcul[position] == "_"){
                 continue;
             }else{
                 return position;
             }
+
             position--;
         }
     }
