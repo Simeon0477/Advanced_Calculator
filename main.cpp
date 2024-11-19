b0aa9b9b6fca796df25a6bea9d330848711a38f3
Siméon0477 <nouhoumimeon01@gmail.com>
2024-11-19 08:46:26 +0100

    commit 1

--- main.cpp
+++ main.cpp
@@ -1,16 +1,50 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
+void operation(char *operation);
+
+int n = 1;
 string chaine;
+string calcul[50];
 
 int main() {
-    cout << "Entre la chaine de caractère : ";
+    cout << "Entrez votre operation : " << endl;
     cin >> chaine;
     char* chaine2 = const_cast<char*>(chaine.c_str());
-
+    operation(chaine2);
+    int i=0;
+    while(calcul[i] != ""){
+        cout << calcul[i] << endl;
+        i++;
+    }
 }
 
-double add()
+void operation(char *operation){
+    int i = 0;
+    char temp[10];
+    //for(i=0; i < taille; i++){
+
+    //}
+    while(*operation != '\0'){
+        if(*operation >= '0' && *operation <= '9'){
+            while(*operation >= '0' && *operation <= '9'){
+                const char constchar[] = {*operation, '\0'};
+                strcat(temp, constchar);
+                operation++;
+            }
+            calcul[i] = temp;
+            i++;
+            memset(temp, '\0', 10);
+        }
+        if(*operation == '+' || *operation == '-' || *operation == '*' || *operation == '/'){
+            calcul[i] = *operation;
+            i++;
+            operation++;
+        }
+    }
+}
+
 
