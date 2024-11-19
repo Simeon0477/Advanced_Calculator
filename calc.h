f972ad8e95dd7381302fa9980e8cf010bb54ecc6
Siméon0477 <nouhoumimeon01@gmail.com>
2024-11-19 03:37:23 +0100

    commit

---
+++ calc.h
@@ -0,0 +1,10 @@
+#ifndef CALCULATRICE_POINTEUR_CALC_H
+#define CALCULATRICE_POINTEUR_CALC_H
+
+int charOperation(char *);
+int charOperationMultiplyOrder(char *);
+int charOperationDivideOrder(char *Operation);
+int charOperationByRespectOrder(char  *operation);
+
+
+#endif 
