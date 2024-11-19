d40dea5d27ca10d544865a18edc7ee23fb738850
Siméon0477 <nouhoumimeon01@gmail.com>
2024-11-19 10:20:45 +0100

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
 void operation(char *operation);
+double calcul_operation(string operation[50]);
+int search(int position, char sens);
 
 int n = 1;
 string chaine;
@@ -16,18 +19,17 @@ int main() {
     char* chaine2 = const_cast<char*>(chaine.c_str());
     operation(chaine2);
     int i=0;
-    while(calcul[i] != ""){
-        cout << calcul[i] << endl;
-        i++;
-    }
+    //while(calcul[i] != ""){
+    //    cout << calcul[i] << endl;
+    //    i++;
+    //}
+
+    cout << "Le resultat est : " << calcul_operation(calcul) << endl;
 }
 
 void operation(char *operation){
     int i = 0;
     char temp[10];
-    //for(i=0; i < taille; i++){
-
-    //}
     while(*operation != '\0'){
         if(*operation >= '0' && *operation <= '9'){
             while(*operation >= '0' && *operation <= '9'){
@@ -47,4 +49,63 @@ void operation(char *operation){
     }
 }
 
+double calcul_operation(string operation[50]){
+    double result;
+    int i=0;
+    while(calcul[i] != ""){
+        if(calcul[i] == " "){
+            continue;
+        }else if(calcul[i] == "*"){
+            result = atof(calcul[search(i, 'd')].c_str()) * atof(calcul[search(i, 'a')].c_str());
+            calcul[search(i, 'd')] = to_string(result);
+            calcul[i] = " ";
+            calcul[search(i, 'a')] = " ";
+        }else if(calcul[i] == "/"){
+            if(atof(calcul[search(i, 'a')].c_str()) == 0){
+                cout << "Math error" << endl;
+            }else{
+                result = atof(calcul[search(i, 'd')].c_str()) / atof(calcul[search(i, 'a')].c_str());
+                calcul[search(i, 'd')] = to_string(result);
+                calcul[i] = " ";
+                calcul[search(i, 'a')] = " ";
+            }
+        }
+        else if(calcul[i] == "+"){
+            result = atof(calcul[search(i, 'd')].c_str()) + atof(calcul[search(i, 'a')].c_str());
+            calcul[search(i, 'd')] = to_string(result);
+            calcul[i] = " ";
+            calcul[search(i, 'a')] = " ";
+        }
+        else if(calcul[i] == "-"){
+            result = atof(calcul[search(i, 'd')].c_str()) - atof(calcul[search(i, 'a')].c_str());
+            calcul[search(i, 'd')] = to_string(result);
+            calcul[i] = " ";
+            calcul[search(i, 'a')] = " ";
+        }
+        i++;
+    }
+    return atof(calcul[0].c_str());
+}
 
+int search(int position, char sens){
+    if(sens == 'a'){
+        while(calcul[position] != ""){
+            if(calcul[position] == " "){
+                continue;
+            }else{
+                return position;
+            }
+            position++;
+        }
+    }else if(sens == 'd'){
+        while(position >= 0){
+            if(calcul[position] == " "){
+                continue;
+            }else{
+                return position;
+            }
+            position--;
+        }
+    }
+    return 0;
+}
